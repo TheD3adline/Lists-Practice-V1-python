554f7f3c3b4cd8d26fe70ae55a24a1aef827147d
MichaelFessler-Work <michaelfessler@gmx.at>
2023-02-22 14:57:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lists-Practice-V1-python
+
+App to read a number of words from user input, store them in a list,
+then print the list to console and give a table of the amount of words
+in the list, how many duplicate entries etc.
---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+    Author: Michael Fessler
+    Date: 2023/02/22
+    Version: 0.1
+    Description:
+            App to read a number of words from user input, store them in a list,
+            then print the list to console and give a table of the amount of words
+            in the list, how many duplicate entries etc.
+"""
+
+wordsList = []
+
+nextInput = str(input("Enter word for the list (or 'CB23' to end input cycle): "))
+if nextInput != "CB23":
+    wordsList.append(nextInput)
+while nextInput != "CB23":
+    nextInput = str(input("Enter word for the list (or 'CB23' to end input cycle): "))
+    if nextInput != "CB23":
+        wordsList.append(nextInput)
+
+wordsList.sort()
+
+wordsSet = (set(wordsList))
+
+
+for word in wordsList:
+    print(word)
+
+for word in wordsSet:
+    print(word)
